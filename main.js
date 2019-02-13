f15df5ec24acef018c810eaaa45985886fdc540b
christina <bergstromcbb@gmail.com>
2019-02-12 21:25:40 -0500

    new project - christina bergstrom

--- main.js
+++ main.js
@@ -1,15 +1,77 @@
-// Renders an error message
-function showError(msg) {
-  const html = `<li><p class="error">${msg}</p></li>`;
-  document.querySelector('#results').innerHTML = html;
-}
+var app = new Vue({
+  el: '#app',
+  data: {
+    query: '',
+    PageTitle: 'I am title',
+    items: [],
+    processing: false
+  },
+  methods: {
+    getAuthorNames(authors) {
+      if (authors)
+        return authors.join(", ")
+      return ""
+    },
+    showError(error) {
+      const html = `<p class="error">${error}</p>`;
+      document.querySelector('#results').innerHTML = html;
+    },
+    render(items) {
+      var html = "<h1 id='result'>Result</h1>";
+      var authors = "";
+      items.forEach(function (item) {
+        var visible = '';
+        if (item.volumeInfo.authors) {
+          authors = item.volumeInfo.authors.join(", ")
+        }
+        if ('subtitle' in item.volumeInfo && item.volumeInfo.subtitle != '') {
+          visible = 'inline-block';
+        } else {
+          visible = 'none';
+        }
+        html += `<div class="cards__item flex-item">
+        <div class="card">
+          <div class="card__image">
+            <img  src="${item.volumeInfo.imageLinks.thumbnail}"
+              alt="${item.volumeInfo.title}">
+          </div>
+          <div class="card__content">
+            <div class="card__title">Title: ${ item.volumeInfo.title }</div>
+            <div class="card__text"> Author: ${authors}</div>
+            <p  style="display:${visible};">
+              Subtitle: ${ item.volumeInfo.subtitle }</p>
+            <a target="_blank" href="${item.volumeInfo.infoLink}">
+              <button class="btn btn--block card__btn">View more about this book</button>
+            </a>
+          </div>
+        </div>
+      </div>`;
+      });
+      document.querySelector('#results').innerHTML = html;
+    },
 
-// Searches for books and returns a promise that resolves a JSON list
-function searchForBooks(term) {
-  // TODO
-}
+    // Searches for books and returns a promise that resolves a JSON list
+    searchForBooks: function () {
+      var self = this
+      if (this.query == "") {
+        alert("Enter Search Query.")
+        return
+      }
+      this.processing = true
+      this.items = []
+      axios.get("https://www.googleapis.com/books/v1/volumes?q=" + this.query)
+        .then(function (response) {
+          if (response.data.totalItems > 0) {
+            self.items = response.data.items
+            self.processing = false;
+            self.render(self.items);
+          };
 
-// Generate HTML and sets #results's contents to it
-function render() {
-  // TODO
-}
+
+        }).catch(function (error) {
+          self.processing = false
+          self.showError('<span id="noresult"> No items found</span>');
+        })
+    }
+  }
+})
